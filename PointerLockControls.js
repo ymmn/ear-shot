5071af7e419ec2291f191ad0e4eaba3addead370
ymmn <abdnimery92@gmail.com>
2014-02-16 07:07:21 -0600

    stuffz

--- PointerLockControls.js
+++ PointerLockControls.js
@@ -226,7 +226,7 @@ THREE.PointerLockControls = function ( camera ) {
 
 	this.update = function ( delta ) {
 
-		var movespeed = 20.12;
+		var movespeed = 6.12;
 		var friction = 0.8;
 
 		if ( scope.enabled === false ) return;
